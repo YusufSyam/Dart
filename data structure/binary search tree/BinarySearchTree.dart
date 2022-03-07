aebdfee4d4635ca1b945dc74c5514871f8de90f9
Yusuf Syam <muhyusufsyamsyam@gmail.com>
2022-03-08 06:34:00 +0800

    Update BinarySearchTree.dart

--- data structure/binary search tree/BinarySearchTree.dart
+++ data structure/binary search tree/BinarySearchTree.dart
@@ -1,14 +1,17 @@
 // Binary Search Tree implementation with dart
 
 import 'TreeNode.dart';
+import 'dart:io';
 
 class BinarySearchTree {
   TreeNode? root = null;
-  String traversalResult = '';
+  int totalNode = 0;
+  int traversedNode = 0;
 
   BinarySearchTree([int? rootData]) {
     if (rootData != null) {
       this.root = TreeNode(rootData);
+      totalNode += 1;
     }
   }
 
@@ -16,6 +19,7 @@ class BinarySearchTree {
     TreeNode? insertNode = TreeNode(data);
     if (this.root == null) {
       this.root = insertNode;
+      totalNode += 1;
     } else {
       TreeNode? currNode = this.root;
       while (true) {
@@ -23,6 +27,7 @@ class BinarySearchTree {
         if (insertNode.data! < currData!) {
           if (currNode.left == null) {
             currNode.left = insertNode;
+            totalNode += 1;
             break;
           } else {
             currNode = currNode.left;
@@ -30,6 +35,7 @@ class BinarySearchTree {
         } else if (insertNode.data! > currData) {
           if (currNode.right == null) {
             currNode.right = insertNode;
+            totalNode += 1;
             break;
           } else {
             currNode = currNode.right;
@@ -48,44 +54,77 @@ class BinarySearchTree {
     }
   }
 
-  void traverse([String? method]) {
+  void search(int searchedData, [String? method]) {
     if (this.root == null) {
       print('There is no data yet');
       return;
+    } else {
+      this.traversedNode = 1;
     }
 
     if (method == null) {
       method = 'postorder';
     }
 
-    this.traversalResult = '';
-
     if (method == 'inorder') {
       // Todo inorder
     } else if (method == 'postorder') {
-      postOrderTraversal(this.root);
+      postOrderTraversal(this.root, 'search', searchedData);
     } else if (method == 'preorder') {
       // Todo preorder
     } else {
       print('Traverse method has been set to postorder traversal');
-      postOrderTraversal(this.root);
+      postOrderTraversal(this.root, 'search', searchedData);
     }
-
-    print(this.traversalResult.substring(0, (this.traversalResult.length - 4)));
   }
 
-  void postOrderTraversal(TreeNode? currNode) {
+  void traverse([String? method]) {
+    if (this.root == null) {
+      print('There is no data yet');
+      return;
+    } else {
+      this.traversedNode = 1;
+    }
+
+    if (method == null) {
+      method = 'postorder';
+    }
+
+    if (method == 'inorder') {
+      // Todo inorder
+    } else if (method == 'postorder') {
+      postOrderTraversal(this.root, 'print');
+    } else if (method == 'preorder') {
+      // Todo preorder
+    } else {
+      print('Traverse method has been set to postorder traversal');
+      postOrderTraversal(this.root, 'print');
+    }
+
+    // print(this.traversalResult.substring(0, (this.traversalResult.length - 4)));
+  }
+
+  void postOrderTraversal([TreeNode? currNode, String? mode, int? search]) {
     TreeNode? left_child = currNode!.left;
     TreeNode? right_child = currNode.right;
 
     if (left_child != null) {
-      postOrderTraversal(left_child);
+      this.traversedNode += 1;
+      postOrderTraversal(left_child, mode, search);
     }
 
     if (right_child != null) {
-      postOrderTraversal(right_child);
+      this.traversedNode += 1;
+      postOrderTraversal(right_child, mode, search);
     }
 
-    this.traversalResult += '${currNode.data} -> ';
+    if (mode == 'search') {
+      if (search == currNode.data) {
+        // Salah ini perbaiki
+        return;
+      }
+    }
+
+    stdout.write('${currNode.data}' + (currNode == this.root ? '\n' : ' -> '));
   }
 }
